54a4952e0a2c8b910e7f295367b855d8a1843412
vidaktomovic <67191628+vidaktomovic@users.noreply.github.com>
2025-04-18 15:19:06 +0200

    Update saveData.js

--- saveData.js
+++ saveData.js
@@ -1,3 +1,21 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/geolocationDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const locationSchema = new mongoose.Schema({
+  latitude: Number,
+  longitude: Number,
+  timestamp: Date
+});
+
+const Location = mongoose.model('Location', locationSchema);
+
+// Inside your POST handler
+const newLocation = new Location({ latitude, longitude, timestamp });
+newLocation.save()
+  .then(() => res.status(200).send('Location data saved.'))
+  .catch(err => res.status(500).send('Error saving location data.'));
+
 // functions/saveData.js
 exports.handler = async (event, context) => {
   const { latitude, longitude, photo } = JSON.parse(event.body);
